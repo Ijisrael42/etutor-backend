2e5c3d31770f9a065c9aef618d82036b375d6c37
Ijudah42 <ijisrael42@gmail.com>
2021-04-07 16:00:53 +0200

    new commit

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ const corsOptions = {
 
 // allow cors requests from any origin and with credentials
 // app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 // api routes
 app.use('/accounts', require('./accounts/accounts.controller'));
@@ -45,7 +46,7 @@ app.use('/api-docs', require('_helpers/swagger'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3000;
 app.listen(port, () => {
     console.log('Server listening on port ' + port);
 });
