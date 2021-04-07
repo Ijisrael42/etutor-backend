601839645417d504d4c31903cf2c5394157cc2bf
Ijudah42 <ijisrael42@gmail.com>
2021-04-07 15:04:41 +0200

    new commit

--- server.js
+++ server.js
@@ -10,8 +10,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+const allowedOrigins = [
+    'capacitor://localhost',
+    'ionic://localhost',
+    'http://localhost',
+    'http://localhost:8080',
+    'http://localhost:8100'
+];
+  
+// Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+        } else {
+        callback(new Error('Origin not allowed by CORS'));
+        }
+    }
+}
+
 // allow cors requests from any origin and with credentials
-app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+// app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+app.use(cors(corsOptions));
 
 // api routes
 app.use('/accounts', require('./accounts/accounts.controller'));
