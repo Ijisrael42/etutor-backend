b5cb1b1076eb068038d78aa8a7263b0d3e7e956e
Ijudah42 <ijisrael42@gmail.com>
2021-05-15 22:45:28 +0200

    2021/05/15

--- accounts/accounts.controller.js
+++ accounts/accounts.controller.js
@@ -220,7 +220,7 @@ function getTutorDeviceTokens(req, res, next) {
 
 function getById(req, res, next) {
     // users can get their own account and admins can get any account
-    if (req.params.id !== req.user.id && req.user.role !== Role.Admin) {
+    if (req.params.id !== req.user.id ) { // && req.user.role !== Role.Admin) {
         return res.status(401).json({ message: 'Unauthorized' });
     }
 
